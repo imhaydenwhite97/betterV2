5b21508ad373af62a8ae34ee14ca730c4845cd1e
imhaydenwhite97 <imhaydenwhite@gmail.com>
2024-12-14 21:02:37 -0800

    Add files via upload

---
+++ better-child/loading-screen/loading-screen.php
@@ -0,0 +1,6 @@
+<div id="loading-screen">
+    <div id="progress-bar"></div>
+    <div class="logo-container">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/better-logo.svg" alt="<?php echo get_bloginfo('name'); ?>">
+    </div>
+</div>
