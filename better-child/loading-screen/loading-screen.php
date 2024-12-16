15c7de820efc1ffab043db327f68f20c51aad9f1
imhaydenwhite97 <imhaydenwhite@gmail.com>
2024-12-15 22:12:44 -0800

    Delete better-child directory

--- better-child/loading-screen/loading-screen.php
+++
@@ -1,6 +0,0 @@
-<div id="loading-screen">
-    <div id="progress-bar"></div>
-    <div class="logo-container">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/better-logo.svg" alt="<?php echo get_bloginfo('name'); ?>">
-    </div>
-</div>
